8b305c150b5372ed8a317235d34c9f03a07b11cd
Emil Niyaz <niyaz.emil@gmail.com>
2018-11-13 23:51:13 -0500

    Get image from local file

--- screenshot-reminder/content.js
+++ screenshot-reminder/content.js
@@ -2,15 +2,22 @@ console.log('Content js');
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
+let a = chrome.runtime.getURL('images/12.png');
+console.log(a);
+
 function gotMessage(msg,sender,sendResponse) {
 	console.log('tabs');
 	console.log(msg)
 	var popup = document.createElement("div");
 	var bg = document.createElement("div");
-	popup.style.cssText = "position: fixed; width: 400px; left: 50%; top: 50%; transform: translate(-50%, -50%); border: 1px solid black; border-radius: 5px; height: 200px; background: white; z-index: 100002;";
+	var img = document.createElement("img");
+	popup.style.cssText = "position: fixed; width: 80%; left: 50%; top: 50%; background-position: center center; transform: translate(-50%, -50%); border: 1px solid black; border-radius: 5px; height: 70%; background: white; z-index: 100002; background-size: cover;";
 	bg.style.cssText = "position: fixed; width: 100%; height: 100%; z-index: 100001; background: black; opacity: .7;";
-
+	console.log(a, '1');
+	popup.style.backgroundImage = `url('${a}')`;
 	document.body.style.padding = "0"
+	img.src = a;
+	// popup.appendChild(img);
 	document.body.appendChild(popup);
 	document.body.insertBefore(bg, document.body.firstChild);
 }
